d0b50de4bf8e4aaa3023eed77af3ec0486a03670
Richard Holmes <rholmes@richardholmes.net>
2011-03-11 19:10:05 -0600

    initial commit - empty core data / document based application for Mac osX.

---
+++ MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  fwerpy
+//
+//  Created by Richard Holmes on 3/11/11.
+//  Copyright Richard A. Holmes 2011 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
---
+++ MyDocument.m
@@ -0,0 +1,33 @@
+//
+//  MyDocument.m
+//  fwerpy
+//
+//  Created by Richard Holmes on 3/11/11.
+//  Copyright Richard A. Holmes 2011 . All rights reserved.
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init 
+{
+    self = [super init];
+    if (self != nil) {
+        // initialization code
+    }
+    return self;
+}
+
+- (NSString *)windowNibName 
+{
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)windowController 
+{
+    [super windowControllerDidLoadNib:windowController];
+    // user interface preparation code
+}
+
+@end
